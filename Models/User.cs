d45d1d251662cb51600687d238c7b0dd0558f661
galaadBigos <galaad.bigos@gmail.com>
2023-05-03 13:58:49 +0200

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace AdminMNS.API.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string Firstname { get; set; }
+		public string Lastname { get; set; }
+		public DateTime Birthday { get; set; }
+		public string Password { get; set; }
+		public string MailAddress { get; set; }
+		public string WayNumber { get; set; }
+		public string WayType { get; set; }
+		public string WayName { get; set; }
+		public string City { get; set; }
+		public string PostalCode { get; set; }
+		public bool IsValidRegistration { get; set; }
+		public int IdUserStatus { get; set; }
+		public int IdGraduatingClass { get; set; }
+	}
+}
---
+++ Models/UserStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminMNS.API.Models
+{
+	public class UserStatus
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
