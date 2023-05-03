afa7064e007024a0185cfcccec6a7323c47681d9
galaadBigos <galaad.bigos@gmail.com>
2023-05-03 22:25:09 +0200

    03/05/2023 : Création de UserRepository (GetUsers et GetUserById)

--- Domain/Services/UserService.cs
+++ Domain/Services/UserService.cs
@@ -1,12 +1,13 @@
 ﻿using AdminMNS.API.Abstractions;
 using AdminMNS.API.Domain.DTO;
 using AdminMNS.API.Models;
+using AdminMNS.API.Repository;
 using Microsoft.Data.SqlClient;
 using System.Data;
 
 namespace AdminMNS.API.Domain.Services
 {
-	public class UserService : IUserService
+    public class UserService : IUserService
 	{
 		private readonly IDbConnection _dbConnection;
 
@@ -17,39 +18,10 @@ namespace AdminMNS.API.Domain.Services
 
 		public IEnumerable<UserItemDTO> GetUserItemDTOs()
 		{
-			List<UserItemDTO> result = new List<UserItemDTO>();
+			UserRepository userRepository = new UserRepository(_dbConnection);
+			IEnumerable<User> users = userRepository.GetUsers();
 
-			string query = "SELECT * FROM [User]";
-
-			_dbConnection.Open();
-			IDbCommand command = _dbConnection.CreateCommand();
-			command.CommandText = query;
-
-			IDataReader reader = command.ExecuteReader();
-
-			while (reader.Read())
-			{
-				User user = new User()
-				{
-					Id = reader.GetInt32(0),
-					Firstname = reader.GetString(1),
-					Lastname = reader.GetString(2),
-					Birthday = reader.GetDateTime(3),
-					Password = reader.GetString(4),
-					MailAddress = reader.GetString(5),
-					WayNumber = reader.GetString(6),
-					WayType = reader.GetString(7),
-					WayName = reader.GetString(8),
-					City = reader.GetString(9),
-					PostalCode = reader.GetString(10),
-					IsValidRegistration = reader.GetBoolean(11),
-					IdUserStatus = reader.GetInt32(12),
-					IdGraduatingClass = reader.GetInt32(13),
-				};
-				result.Add(new UserItemDTO(user));
-			}
-			_dbConnection.Close();
-			return result;
+			return users.Select(user => new UserItemDTO(user)).ToList();
 		}
 
 		public UserItemDTO GetUserItemDTO(int id)
--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -1,7 +1,90 @@
-﻿namespace AdminMNS.API.Repository
-{
-	public class UserRepository
-	{
+﻿using AdminMNS.API.Domain.DTO;
+using AdminMNS.API.Models;
+using AdminMNS.API.Repository.Abstractions;
+using System.Data;
 
-	}
+namespace AdminMNS.API.Repository
+{
+    public class UserRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public UserRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            List<User> result = new List<User>();
+
+            string query = "SELECT * FROM [User]";
+
+            _dbConnection.Open();
+            IDbCommand command = _dbConnection.CreateCommand();
+            command.CommandText = query;
+
+            IDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                User user = new User()
+                {
+                    Id = reader.GetInt32(0),
+                    Firstname = reader.GetString(1),
+                    Lastname = reader.GetString(2),
+                    Birthday = reader.GetDateTime(3),
+                    Password = reader.GetString(4),
+                    MailAddress = reader.GetString(5),
+                    WayNumber = reader.GetString(6),
+                    WayType = reader.GetString(7),
+                    WayName = reader.GetString(8),
+                    City = reader.GetString(9),
+                    PostalCode = reader.GetString(10),
+                    IsValidRegistration = reader.GetBoolean(11),
+                    IdUserStatus = reader.GetInt32(12),
+                    IdGraduatingClass = reader.GetInt32(13),
+                };
+                result.Add(user);
+            }
+            _dbConnection.Close();
+            return result;
+        }
+
+        public User GetUserById(int id)
+        {
+            User? result = null;
+
+            string query = $"SELECT * FROM [User] WHERE Id_user = {id}";
+
+            _dbConnection.Open();
+            IDbCommand command = _dbConnection.CreateCommand();
+            command.CommandText = query;
+
+            IDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                result = new User()
+                {
+                    Id = reader.GetInt32(0),
+                    Firstname = reader.GetString(1),
+                    Lastname = reader.GetString(2),
+                    Birthday = reader.GetDateTime(3),
+                    Password = reader.GetString(4),
+                    MailAddress = reader.GetString(5),
+                    WayNumber = reader.GetString(6),
+                    WayType = reader.GetString(7),
+                    WayName = reader.GetString(8),
+                    City = reader.GetString(9),
+                    PostalCode = reader.GetString(10),
+                    IsValidRegistration = reader.GetBoolean(11),
+                    IdUserStatus = reader.GetInt32(12),
+                    IdGraduatingClass = reader.GetInt32(13),
+                };
+            }
+            _dbConnection.Close();
+            return result;
+        }
+    }
 }
